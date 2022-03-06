06ddd7ade0f9dddc0caf487a13f82cee97a54f2d
christopherdal <christopherdallar1234@gmail.com>
2022-03-06 17:24:37 -0400

    Tarea y Final de seccion primera app en react handleSubtract y handleReset

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -1,28 +1,32 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0); // [].length == 2
+const CounterApp = ({ value = 10 }) => {
+  const [counter, setCounter] = useState(value); // [].length == 2
 
-  console.log(counter);
-
-  //
+  // Handle
   const handleAdd = () => {
     // setCounter(counter + 1); // Is equal to code bellow
     setCounter((c) => c + 1);
   };
+
+  const handleSubtract = () => setCounter(counter - 1);
+
+  const handleReset = () => setCounter(value);
 
   return (
     <>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSubtract}>-1</button>
     </>
   );
 };
 
 CounterApp.propTypes = {
-  value: PropTypes.number.isRequired,
+  value: PropTypes.number,
 };
 
 export default CounterApp;
