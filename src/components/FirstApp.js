0fd893fbd5cd6a58d4d578dac65464ccbdf454f3
christopherdal <christopherdallar1234@gmail.com>
2022-03-06 16:27:15 -0400

    DefaultProps

--- src/components/FirstApp.js
+++ src/components/FirstApp.js
@@ -3,18 +3,24 @@ import PropTypes from 'prop-types';
 
 // props means properties
 
-const FirstApp = ({ regard }) => {
+const FirstApp = ({ regard, subtitle }) => {
   return (
     <>
       <h1>{regard} World</h1>
       <p>Mi primera app</p>
+      <p>{subtitle}</p>
     </>
   );
 };
 
 FirstApp.propTypes = {
   regard: PropTypes.string.isRequired,
+  subtitle: PropTypes.any,
   // other: PropTypes.number.isRequired
+};
+FirstApp.defaultProps = {
+  subtitle: '',
+  // subtitle: 'Soy un subtitle',
 };
 
 export default FirstApp;
