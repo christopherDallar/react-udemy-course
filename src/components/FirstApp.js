ed3969905ce82408312420c3512644c8fe6e0322
christopherdal <christopherdallar1234@gmail.com>
2022-03-06 16:04:44 -0400

    Interpolation

--- src/components/FirstApp.js
+++ src/components/FirstApp.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+const regard = 'Saludo';
+// const regard = ['Saludo 1', 'Saludo 2']; //Work
+// const regard = false; Not work
+// const regard = { text: ''}; Not work
+
 const FirstApp = () => {
   return (
     <>
-      <h1>Hello World ssssssssssssss</h1>
+      <h1>{regard} World</h1>
       <p>Mi primera app</p>
     </>
   );
