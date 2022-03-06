04ba58dfadaf357167682993d851787b4f29d35d
christopherdal <christopherdallar1234@gmail.com>
2022-03-06 15:59:11 -0400

    Mi primer componente

---
+++ src/components/FirstApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FirstApp = () => {
+  return (
+    <>
+      <h1>Hello World ssssssssssssss</h1>
+      <p>Mi primera app</p>
+    </>
+  );
+};
+
+export default FirstApp;
