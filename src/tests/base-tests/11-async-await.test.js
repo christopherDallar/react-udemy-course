70f0f2a08fb94e115b2ef89e55a2ce89e1c4c009
christopherdal <christopherdallar1234@gmail.com>
2022-04-21 20:22:04 -0400

    58. Pruebas con async-await

---
+++ src/tests/base-tests/11-async-await.test.js
@@ -0,0 +1,15 @@
+import { getImagen } from './../../base-tests/11-async-await';
+
+describe('Testing into File 11-async-await.test.js', () => {
+  test('Should return picture url string', async () => {
+    const url = await getImagen();
+
+    expect(typeof url).toBe('string');
+  });
+
+  test('Should return picture url http://', async () => {
+    const url = await getImagen();
+
+    expect(url.includes('https://')).toBe(true);
+  });
+});
