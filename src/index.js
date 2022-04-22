48f84ce69b58f2b48d962074740ac691499f79f4
christopherdal <christopherdallar1234@gmail.com>
2022-04-21 20:44:38 -0400

    Pruebas sobre componentes de React
    Prueba sin Enzime

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CounterApp from './components/CounterApp';
+// import CounterApp from './components/CounterApp';
 import './index.css';
+import FirstApp from './components/FirstApp';
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<CounterApp value={123} />, divRoot);
+ReactDOM.render(
+  <FirstApp regard={'Hello, I am Goku'} />,
+  divRoot,
+);
+// ReactDOM.render(<CounterApp value={123} />, divRoot);
