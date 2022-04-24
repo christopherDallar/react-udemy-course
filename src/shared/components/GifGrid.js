9187714259e3fe35e0e1bfe226065fac3e1ad477
christopherdal <christopherdallar1234@gmail.com>
2022-04-24 17:26:49 -0400

    Detener 76. useEffect

--- src/shared/components/GifGrid.js
+++ src/shared/components/GifGrid.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const GifGrid = ({ category }) => {
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		getGifs();
+	}, []);
+
 	const getGifs = async () => {
 		const url =
 			'https://api.giphy.com/v1/gifs/search?q=Rick and&limit=10&api_key=4PcTaskj1GEQj0JDFKX59fsQ6RKYDlUH';
-
 		const resp = await fetch(url);
-
 		const { data } = await resp.json();
 
 		const gifs = data.map((img) => {
@@ -20,11 +24,10 @@ export const GifGrid = ({ category }) => {
 		console.log(gifs);
 	};
 
-	getGifs();
-
 	return (
 		<>
-			<h3>{category}</h3>
+			<h3>{count}</h3>
+			<button onClick={() => setCount(count + 1)}>Add</button>
 		</>
 	);
 };
