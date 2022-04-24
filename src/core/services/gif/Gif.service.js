de3df9133944137670968c995b5f359c17a3a4e9
christopherdal <christopherdallar1234@gmail.com>
2022-04-24 19:54:59 -0400

    79. Helpers - getGifs Using my own file system

---
+++ src/core/services/gif/Gif.service.js
@@ -0,0 +1,26 @@
+const apiUrl = 'https://api.giphy.com/v1/gifs';
+const apiKey = '4PcTaskj1GEQj0JDFKX59fsQ6RKYDlUH';
+
+const getGifs = async (category, setState) => {
+	const url = `${apiUrl}/search?q=${encodeURI(
+		category
+	)}&limit=10&api_key=${apiKey}`;
+	const resp = await fetch(url);
+	const { data } = await resp.json();
+
+	const gifs = data.map((img) => {
+		return {
+			id: img.id,
+			title: img.title,
+			url: img.images.downsized_medium.url,
+		};
+	});
+
+	setState(gifs);
+};
+
+const GifService = {
+	getGifs,
+};
+
+export default GifService;
--- src/shared/components/GifGrid.js
+++ src/shared/components/GifGrid.js
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { GifGridItem } from './GifGridItem';
+import GifService from './../../core/services/gif/Gif.service';
 
 export const GifGrid = ({ category }) => {
 	const [images, setImages] = useState([]);
 
 	useEffect(() => {
-		getGifs();
-	}, []);
+		// const getGifs = async () => {
+		// 	const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+		// 		category
+		// 	)}&limit=10&api_key=4PcTaskj1GEQj0JDFKX59fsQ6RKYDlUH`;
+		// 	const resp = await fetch(url);
+		// 	const { data } = await resp.json();
 
-	const getGifs = async () => {
-		const url =
-			'https://api.giphy.com/v1/gifs/search?q=one punch&limit=10&api_key=4PcTaskj1GEQj0JDFKX59fsQ6RKYDlUH';
-		const resp = await fetch(url);
-		const { data } = await resp.json();
+		// 	const gifs = data.map((img) => {
+		// 		return {
+		// 			id: img.id,
+		// 			title: img.title,
+		// 			url: img.images.downsized_medium.url,
+		// 		};
+		// 	});
 
-		const gifs = data.map((img) => {
-			return {
-				id: img.id,
-				title: img.title,
-				url: img.images.downsized_medium.url,
-			};
-		});
+		// 	setImages(gifs);
+		// };
 
-		setImages(gifs);
-	};
+		GifService.getGifs(category, setImages);
+	}, [category]);
 
 	return (
 		<>
