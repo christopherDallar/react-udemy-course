c7091ae758e8e52e060fce606f0904e426f4020c
christopherdal <christopherdallar1234@gmail.com>
2022-04-24 17:43:17 -0400

    77. Mostrar los títulos de las imágenes

--- src/shared/components/GifGrid.js
+++ src/shared/components/GifGrid.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-	const [count, setCount] = useState(0);
+	const [images, setImages] = useState([]);
 
 	useEffect(() => {
 		getGifs();
@@ -21,13 +22,16 @@ export const GifGrid = ({ category }) => {
 			};
 		});
 
-		console.log(gifs);
+		setImages(gifs);
 	};
 
 	return (
 		<>
-			<h3>{count}</h3>
-			<button onClick={() => setCount(count + 1)}>Add</button>
+			<h3>{category}</h3>
+
+			{images.map((img) => (
+				<GifGridItem key={img.id} {...img} />
+			))}
 		</>
 	);
 };
