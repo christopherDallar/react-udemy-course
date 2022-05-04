2bd8407e6a0b4f65be268fae8bb0d751c0d15086
christopherdal <christopherdallar1234@gmail.com>
2022-05-04 03:18:04 -0400

    92. Pruebas del componente - GifGridItem

--- src/tests/GifGridItem.test.js
+++ src/tests/GifGridItem.test.js
@@ -7,9 +7,26 @@ describe('Test on GiftGridItem Component', () => {
 	const title = 'Un titulo';
 	const url = 'https://localhost/';
 
-	let wrapper = shallow(<GifGridItem title={title} url={url} />);
+	const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
 	test('should Match With Snapshot GiftGridItem Correctly', () => {
 		expect(wrapper).toMatchSnapshot(); // To Save the literal rendered component and compare with wrapper if you press 'u' update the snapshot
+	});
+
+	test(`should has the same ${title} in tag <p> `, () => {
+		const p = wrapper.find('p');
+		expect(p.text().trim()).toBe(title);
+	});
+
+	test('should has the same src and alt of <img>', () => {
+		const img = wrapper.find('img');
+		expect(img.prop('src')).toBe(url);
+		expect(img.prop('alt')).toBe(title);
+	});
+
+	test('should has class in <div> the className  animate__bounce', () => {
+		const div = wrapper.find('div');
+		const className = 'animate__bounce';
+		expect(div.hasClass(className)).toBe(true);
 	});
 });
