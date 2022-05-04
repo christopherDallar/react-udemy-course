b955c76e7258aaf18625988f0f4e8df2969a9ec9
christopherdal <christopherdallar1234@gmail.com>
2022-05-03 21:46:58 -0400

    Detener 91. Implementando PropTypes

--- src/tests/GifGridItem.test.js
+++ src/tests/GifGridItem.test.js
@@ -4,7 +4,10 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from './../shared/components/gif/GifGridItem.component';
 
 describe('Test on GiftGridItem Component', () => {
-	let wrapper = shallow(<GifGridItem />);
+	const title = 'Un titulo';
+	const url = 'https://localhost/';
+
+	let wrapper = shallow(<GifGridItem title={title} url={url} />);
 
 	test('should Match With Snapshot GiftGridItem Correctly', () => {
 		expect(wrapper).toMatchSnapshot(); // To Save the literal rendered component and compare with wrapper if you press 'u' update the snapshot
