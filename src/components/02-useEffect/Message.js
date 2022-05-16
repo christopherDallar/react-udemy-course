38206402b4279c7dcc96f93f776d46e93f68d90f
christopherdal <christopherdallar1234@gmail.com>
2022-05-16 00:08:17 -0400

    111. useEffect - Precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
+	const [{ x, y }, setcoords] = useState({ x: 0, y: 0 });
+
 	useEffect(() => {
+		const mouseMove = (e) => {
+			const coords = { x: e.x, y: e.y };
+
+			setcoords(coords);
+			console.log(' :D ');
+		};
+
+		window.addEventListener('mousemove', mouseMove);
+
 		console.log('componente montado');
 
 		return () => {
 			console.log('componente desmontado');
+			window.removeEventListener('mousemove', mouseMove);
 		};
 	}, []);
 
 	return (
 		<>
 			<h3>Eres genial</h3>
+			<p>
+				x:{x} y y:{y}
+			</p>
 		</>
 	);
 };
