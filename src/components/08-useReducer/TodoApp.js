5a7c18cd0d61bcf08583d1ad486f95225ef01e71
christopherdal <christopherdallar1234@gmail.com>
2022-05-28 15:53:20 -0400

    128. Creando el cascarón de la lista de TODOs

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -15,15 +15,42 @@ export const TodoApp = () => {
 	console.log(todos);
 
 	return (
-		<div>
-			<h1>TodoApp</h1>
+		<div className='todoApp'>
+			<h1>TodoApp ( {todos.length} )</h1>
 			<hr />
 
-			<ul>
-				<li>Item 1</li>
-				<li>Item 2</li>
-				<li>Item 3</li>
-			</ul>
+			<div className='row'>
+				<div className='col-7'>
+					<ul className='list-group list-group-flush'>
+						{todos.map((todo, idx) => (
+							<li className='list-group-item' key={todo.id}>
+								<p className='text-center'>
+									{idx + 1}.{todo.desc}
+								</p>
+								<button className='btn btn-danger'>Delete</button>
+							</li>
+						))}
+					</ul>
+				</div>
+
+				<div className='col-5'>
+					<h4>Agregar TODO</h4>
+					<hr />
+
+					<form>
+						<input
+							type='text'
+							name='description'
+							placeholder='Learn...'
+							className='form-control'
+						/>
+
+						<button className='btn btn-outline-primary mt-1 btn-block'>
+							Add
+						</button>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
