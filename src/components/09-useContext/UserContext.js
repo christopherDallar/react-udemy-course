36521b4e98e3667d6251f6d777da54dbbdbda7f2
christopherdal <christopherdallar1234@gmail.com>
2022-05-29 16:36:36 -0400

    142. CreateContext y ContextProvider

---
+++ src/components/09-useContext/UserContext.js
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+export const UserContext = createContext(null);
+
+// export const UserContext = () => {
+//   return (
+//     <div>UserContext</div>
+//   )
+// }
--- src/components/09-useContext/atomic/Navbar.js
+++ src/components/09-useContext/atomic/Navbar.js
@@ -3,20 +3,6 @@ import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
 	return (
-		// <nav>
-		// 	<ul>
-		// 		<li>
-		// 			<Link to='/'>Home</Link>
-		// 		</li>
-		// 		<li>
-		// 			<Link to='/about'>About</Link>
-		// 		</li>
-		// 		<li>
-		// 			<Link to='/login'>Login</Link>
-		// 		</li>
-		// 	</ul>
-		// </nav>
-
 		<nav className='navbar navbar-expand-sm bg-dark'>
 			<div className='container-fluid'>
 				<Link to='/' className='navbar-brand'>
@@ -24,13 +10,13 @@ export const Navbar = () => {
 				</Link>
 				<div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
 					<div className='navbar-nav'>
-						<NavLink activeClassName='active' to='/' className='nav-link'>
+						<NavLink activeclassname='active' to='/' className='nav-link'>
 							Home
 						</NavLink>
-						<NavLink activeClassName='active' to='/about' className='nav-link'>
+						<NavLink activeclassname='active' to='/about' className='nav-link'>
 							About
 						</NavLink>
-						<NavLink activeClassName='active' to='/login' className='nav-link'>
+						<NavLink activeclassname='active' to='/login' className='nav-link'>
 							Login
 						</NavLink>
 					</div>
