f5a65145dc2c55de55e62bd8e9ec2364a28cdd33
christopherdallar <christopherdallar1234@gmail.com>
2022-06-01 23:30:49 -0400

    177. Lista de Heroes

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { HeroList } from './../hero/HeroList';
 
 export const DcScreen = () => {
+	const publisher = 'DC Comics';
+
 	return (
 		<div>
-			<h1>DcScreen</h1>
+			<h1>{publisher}</h1>
+			<hr />
+
+			<HeroList publisher={publisher} />
 		</div>
 	);
 };
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -11,15 +11,17 @@ export const DashboardRoutes = () => {
 		<>
 			<Navbar />
 
-			<Routes>
-				<Route path='marvel' element={<MarvelScreen />} />
-				<Route path='dc' element={<DcScreen />} />
+			<div className='container'>
+				<Routes>
+					<Route path='marvel' element={<MarvelScreen />} />
+					<Route path='dc' element={<DcScreen />} />
 
-				<Route path='search' element={<SearchScreen />} />
-				<Route path='/heroe' element={<HeroScreen />} />
+					<Route path='search' element={<SearchScreen />} />
+					<Route path='/heroe' element={<HeroScreen />} />
 
-				<Route path='/' element={<MarvelScreen />} />
-			</Routes>
+					<Route path='/' element={<MarvelScreen />} />
+				</Routes>
+			</div>
 		</>
 	);
 };
