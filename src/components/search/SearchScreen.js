1c86b61965f294ada12975860f63ce5b5eda6ad0
christopherdallar <christopherdallar1234@gmail.com>
2022-06-03 06:28:48 -0400

    Detener 183. ** SearchComponent

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useForm } from './../../hooks/useForm';
 
 export const SearchScreen = () => {
+	const [{ searchText }, handleInputChange, reset] = useForm({
+		searchText: '',
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log(searchText);
+
+		reset();
+	};
+
 	return (
-		<div>
-			<h1>SearchScreen</h1>
-		</div>
+		<>
+			<h1>Searcher</h1>
+			<hr />
+
+			<div className='row'>
+				<div className='col-5'>
+					<h4>Search</h4>
+
+					<form onSubmit={handleSubmit}>
+						<input
+							type='text'
+							placeholder='Search a hero'
+							className='form-control'
+							name='searchText'
+							autoComplete='off'
+							onChange={handleInputChange}
+							value={searchText}
+						/>
+
+						<button className='btn btn-outline-primary mt-1' type='submit'>
+							Search...
+						</button>
+					</form>
+				</div>
+			</div>
+		</>
 	);
 };
