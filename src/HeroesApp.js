0423eac56c72ef6e49db3132d90015173bf451f2
christopherdallar <christopherdallar1234@gmail.com>
2022-06-05 15:44:44 -0400

    193. * Proveer estado global de la aplicación

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { AppRouter } from './routers/AppRouter';
 import { AuthContext } from './auth/authContext';
+import { authReducer } from './auth/authReducer';
+
+const init = () => {
+	return JSON.parse(localStorage.getItem('user')) || { logged: false };
+};
 
 export const HeroesApp = () => {
+	const [user, dispatch] = useReducer(authReducer, {}, init);
+
 	return (
-		<>
-			<AuthContext.Provider value={null}>
-				<AppRouter />
-			</AuthContext.Provider>
-		</>
+		<AuthContext.Provider
+			value={{
+				user,
+				dispatch,
+			}}
+		>
+			<AppRouter />
+		</AuthContext.Provider>
 	);
 };
