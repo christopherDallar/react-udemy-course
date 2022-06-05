6bee0d928563aa448ac295e2c2910f4006c7804a
christopherdallar <christopherdallar1234@gmail.com>
2022-06-05 14:44:28 -0400

    Reproducir 192. Context y Reducer de mi aplicación

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { AppRouter } from './routers/AppRouter';
+import { AuthContext } from './auth/authContext';
 
 export const HeroesApp = () => {
-	return <AppRouter />;
+	return (
+		<>
+			<AuthContext.Provider value={null}>
+				<AppRouter />
+			</AuthContext.Provider>
+		</>
+	);
 };
