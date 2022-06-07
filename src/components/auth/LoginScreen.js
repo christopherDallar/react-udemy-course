aa541479dabd2ef1f437d5a4c835581440aff07c
christopherdallar <christopherdallar1234@gmail.com>
2022-06-07 01:11:43 -0400

    230. Estilos del AuthRouter

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,15 @@ import React from 'react';
 export const LoginScreen = () => {
 	return (
 		<div>
-			<h1>LoginScreen</h1>
+			<h3>Login</h3>
+
+			<form>
+				<input type='email' placeholder='Email' name='email' />
+				<input type='password' placeholder='Password' name='password' />
+				<button type='submit'>Login</button>
+				<hr />
+				google
+			</form>
 		</div>
 	);
 };
