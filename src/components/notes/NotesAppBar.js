6eba42dd7b367fb22155e3e3f26ae243c96a0d07
christopherdallar <christopherdallar1234@gmail.com>
2022-06-12 10:34:49 -0400

    236. NoteAppBar

---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const NotesAppBar = () => {
+	return (
+		<div className='notes__appBar'>
+			<span>28 august 2022 </span>
+
+			<div>
+				<button className='btn'>Picture</button>
+				<button className='btn'>Save</button>
+			</div>
+		</div>
+	);
+};
