8e6f9b7192cddbda2deabf8eef5ab3118a39c422
christopherdallar <christopherdallar1234@gmail.com>
2022-06-12 13:37:12 -0400

    245. Configurando Redux en nuestra aplicación

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { JournalApp } from './JournalApp';
 import './styles/styles.scss';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<JournalApp />);
+root.render(
+	<Provider store={store}>
+		<JournalApp />
+	</Provider>
+);
 // Removed the following line, because it was causing a problem with the Router Link:
 // <React.StrictMode>
 // </React.StrictMode>
