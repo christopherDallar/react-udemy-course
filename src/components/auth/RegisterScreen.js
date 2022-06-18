04d4e3daf0dff8c84c2be930ed961f595d49587e
christopherdallar <christopherdallar1234@gmail.com>
2022-06-18 13:57:19 -0400

    276. Formulario de registro de usuarios

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,39 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from './../../hooks';
+
+const formData = {
+	displayName: 'Christopher Dallar',
+	email: 'christopher@google.com',
+	password: '123456',
+	passwordConfirm: '',
+};
 
 export const RegisterScreen = () => {
+	const {
+		displayName,
+		email,
+		password,
+		passwordConfirm,
+		onInputChange,
+		formState,
+	} = useForm(formData);
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(formState);
+	};
+
 	return (
 		<>
 			<h3 className='auth__title'>Register</h3>
 
-			<form>
+			<form onSubmit={onSubmit}>
 				<input
 					type='text'
 					placeholder='name'
-					name='name'
 					className='auth__input'
 					autoComplete='off'
+					name='displayName'
+					onChange={onInputChange}
+					value={displayName}
 				/>
 
 				<input
 					type='email'
 					placeholder='Email'
-					name='email'
 					className='auth__input'
 					autoComplete='off'
+					name='email'
+					onChange={onInputChange}
+					value={email}
 				/>
 				<input
 					type='password'
 					placeholder='Password'
-					name='password'
 					className='auth__input'
+					name='password'
+					onChange={onInputChange}
+					value={password}
 				/>
 
 				<input
 					type='password'
 					placeholder='Confirm password'
-					name='password_confirm'
 					className='auth__input'
+					name='passwordConfirm'
+					onChange={onInputChange}
+					value={passwordConfirm}
 				/>
 
 				<button type='submit' className='btn btn-primary btn-block mb-5'>
