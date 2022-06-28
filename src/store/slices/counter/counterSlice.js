d98ab107c665f488e23f2cc084b9e024f3daeeb2
christopherdallar <christopherdallar1234@gmail.com>
2022-06-27 20:17:41 -0400

    255. ConfigureStore y Slices

---
+++ src/store/slices/counter/counterSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const counterSlice = createSlice({
+	name: 'counter',
+	initialState: {
+		value: 10,
+	},
+	reducers: {
+		increment: (state) => {
+			state.counter++;
+		},
+	},
+});
+
+// Action creators are generated for each case reducer function
+export const { increment } = counterSlice.actions;
