a53632f77413155215e3ec93bed1f0e0677d4fd8
christopherdallar <christopherdallar1234@gmail.com>
2022-07-02 12:33:38 -0400

    263. RTK Query

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 // import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { PokemonApp } from './PokemonApp';
+// import { PokemonApp } from './PokemonApp';
+import { TodoApp } from './TodoApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			{/* <App /> */}
-			<PokemonApp />
+			{/* <PokemonApp /> */}
+			<TodoApp />
 		</Provider>
 	</React.StrictMode>
 );
