0086b06b03138dd92e937b91ff406fff49692fff
christopherdallar <christopherdallar1234@gmail.com>
2022-07-02 17:36:15 -0400

    Detener 296. Crear una nueva nota

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,14 @@
+import { collection, doc, setDoc } from 'firebase/firestore/lite';
+import { FirebaseDB } from './../../firebase/config';
+
 export const startNewNote = () => {
-	return async (dispatch) => {
+	return async (dispatch, getState) => {
 		// uid
+
+		const { uid } = getState().auth;
+		console.log('====================================');
+		console.log(getState());
+		console.log('====================================');
 
 		console.log('====================================');
 		console.log('startNewNote');
@@ -11,6 +19,9 @@ export const startNewNote = () => {
 			body: '',
 			date: new Date().getTime(),
 		};
+
+		const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
+		await setDoc(newDoc, newNote);
 
 		// dispatch
 		// dispatch( newNote )
