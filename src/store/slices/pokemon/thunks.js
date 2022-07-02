e15b6939a748d10afa7c627215bc58c677df7184
christopherdallar <christopherdallar1234@gmail.com>
2022-07-02 11:34:08 -0400

    261. Axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,14 +1,19 @@
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
+import { pokemonApi } from './../../../api/pokemonApi';
 
 export const getPokemons = (page = 0) => {
 	return async (dispatch, getState) => {
 		dispatch(startLoadingPokemons());
 
 		// Realizar petición http
-		const resp = await fetch(
-			`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+		// const resp = await fetch(
+		// 	`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+		// );
+		// const data = await resp.json();
+
+		const { data } = await pokemonApi.get(
+			`/pokemon?limit=10&offset=${page * 10}`
 		);
-		const data = await resp.json();
 
 		console.log(data);
 
