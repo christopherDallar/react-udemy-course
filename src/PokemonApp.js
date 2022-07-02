92d2a5832a1a0ecfb7c44a711bc3582d761326d7
christopherdallar <christopherdallar1234@gmail.com>
2022-07-02 09:50:43 -0400

    Detener 259. pokemonSlice

---
+++ src/PokemonApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const PokemonApp = () => {
+	return (
+		<>
+			<h1>Pokemon App</h1>
+			<hr />
+
+			<ul>
+				<li>Hello</li>
+				<li>Hello</li>
+				<li>Hello</li>
+			</ul>
+		</>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { PokemonApp } from './PokemonApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			{/* <App /> */}
+			<PokemonApp />
 		</Provider>
 	</React.StrictMode>
 );
