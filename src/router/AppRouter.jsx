bddccfad570abb1d1b462b92db884583a7e5ae57
christopherdallar <christopherdallar1234@gmail.com>
2022-07-16 11:35:13 -0400

    342. Rutas de la aplicación

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { LoginPage } from './../auth';
+import { CalendarPage } from './../calendar';
+
+export const AppRouter = () => {
+	const authStatus = 'not-authenticated'; // 'not-authenticated', 'authenticated'
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				{authStatus !== 'authenticated' ? (
+					<Route path='/auth/*' element={<LoginPage />} />
+				) : (
+					<Route path='/*' element={<CalendarPage />} />
+				)}
+
+				{/* <Route path='/*' element={<CalendarPage />} /> */}
+
+				<Route path='/*' element={<Navigate to='/auth/login' />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
