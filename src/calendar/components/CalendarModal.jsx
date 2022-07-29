a03d190e115234b034dc5766ffebe83f317a8762
christopherDallar <christopherdallar@gmail.com>
2022-07-29 01:21:35 -0400

    349. Contenido del Modal

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,38 +1,114 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
+import Modal from 'react-modal'
+import React, { useState } from 'react'
+import { addHours } from 'date-fns'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 const customStyles = {
-	content: {
-		top: '50%',
-		left: '50%',
-		right: 'auto',
-		bottom: 'auto',
-		marginRight: '-50%',
-		transform: 'translate(-50%, -50%)',
-	},
-};
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+}
 
-Modal.setAppElement('#root');
+Modal.setAppElement('#root')
 
 export const CalendarModal = () => {
-	const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true)
 
-	const onCloseModal = () => {
-		setIsOpen(false);
-	};
+  const [formValues, setFormValues] = useState({
+    title: 'Christopher',
+    notes: 'Dallar',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+  })
 
-	return (
-		<Modal
-			isOpen={isOpen}
-			onRequestClose={onCloseModal}
-			style={customStyles}
-			className='modal'
-			overlayClassName='modal-fondo'
-			closeTimeoutMS={200}
-		>
-			<h1>Hello World</h1>
-			<hr />
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-		</Modal>
-	);
-};
+  const onCloseModal = () => {
+    setIsOpen(false)
+  }
+
+  const onInputChanged = ({ target }) => {
+    setFormValues({ ...formValues, [target.name]: formValues[target.value] })
+  }
+
+  const onDateChanged = (event, changing) => {
+    setFormValues({ ...formValues, [changing]: event })
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onCloseModal}
+      style={customStyles}
+      className="modal"
+      overlayClassName="modal-fondo"
+      closeTimeoutMS={200}
+    >
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form className="container">
+        <div className="form-group mb-2">
+          <label>Fecha y hora inicio</label>
+          <DatePicker
+            className="form-control"
+            selected={formValues.start}
+            onChange={(event) => onDateChanged(event, 'start')}
+            dateFormat="Pp"
+          />
+        </div>
+
+        <div className="form-group mb-2">
+          <label>Fecha y hora fin</label>
+          <DatePicker
+            className="form-control"
+            minDate={formValues.start}
+            selected={formValues.end}
+            onChange={(event) => onDateChanged(event, 'end')}
+            dateFormat="Pp"
+          />
+        </div>
+
+        <hr />
+        <div className="form-group mb-2">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+            value={formValues.title}
+            onChange={onInputChanged}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={formValues.notes}
+            onChange={onInputChanged}
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
+    </Modal>
+  )
+}
