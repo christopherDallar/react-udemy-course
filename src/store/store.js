21e4217f56de74b45bfd464bfa9cb1f98a1c6626
christopherDallar <christopherdallar@gmail.com>
2022-08-07 13:58:08 -0400

    361. Redux - serializableCheck

--- src/store/store.js
+++ src/store/store.js
@@ -8,4 +8,8 @@ export const store = configureStore({
     ui: uiSlice.reducer,
     calendar: calendarSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
