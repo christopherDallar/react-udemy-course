f1d198f93b2f1f2e88545753b8589d880b1556b1
christopherDallar <christopherdallar@gmail.com>
2022-08-07 15:26:00 -0400

    367. Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello worldsss')
+const express = require('express')
+
+const app = express()
+
+// Routes
+app.get('/', (red, res) => {
+  res.json({
+    ok: true,
+  })
+})
+
+app.listen(4000, 'localhost', () => {
+  console.log(`server running on port ${4000}`)
+})
