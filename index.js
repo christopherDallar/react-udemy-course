2f694feb1189a074a0488f15a0a17ea8f73b4a70
christopherDallar <christopherdallar@gmail.com>
2022-08-07 15:36:01 -0400

    368. Variables de entorno y carpeta pública

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+require('dotenv').config()
+
+console.log(process.env)
 
 const app = express()
 
-// Routes
-app.get('/', (red, res) => {
-  res.json({
-    ok: true,
-  })
-})
+// Public directory
+app.use(express.static('public'))
 
-app.listen(4000, 'localhost', () => {
-  console.log(`server running on port ${4000}`)
+// Routes
+// app.get('/', (red, res) => {
+//   res.json({
+//     ok: true,
+//   })
+// })
+
+app.listen(process.env.PORT, 'localhost', () => {
+  console.log(`server running on port ${process.env.PORT}`)
 })
