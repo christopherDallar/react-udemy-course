8f3dd789940b25ee694410b5245a0dfef81fdbe9
christopherDallar <christopherdallar@gmail.com>
2022-08-08 19:59:32 -0400

    376. Crear un usuario en nuestra Base de Datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,30 @@
 // https://www.restapitutorial.com/httpstatuscodes.html
 const { response } = require('express') // just for keep intellisense
-const { validationResult } = require('express-validator')
+const User = require('../models/User')
 
 // express.response = just for keep intellisense
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body
+const createUser = async (req, res = response) => {
+  // const { name, email, password } = req.body
 
-  res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password,
-  })
+  const user = new User(req.body) // Automatically mongoose ignore all data not declare on model
+
+  try {
+    await user.save()
+
+    res.status(201).json({
+      ok: true,
+      msg: 'register',
+      // name,
+      // email,
+      // password,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Please contact with support',
+    })
+  }
 }
 
 const loginUser = (req, res = response) => {
