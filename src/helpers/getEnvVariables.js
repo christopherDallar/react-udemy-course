a8c7fc182692b59908d7a5ab7fe861c89a61e7c3
christopherDallar <christopherdallar@gmail.com>
2022-08-20 20:18:06 -0400

    427. Generar versión de producción y desplegarla en Heroku

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,6 +1,7 @@
 export const getEnvVariables = () => {
-  import.meta.env // It's wear but to use env has to write twice times here and
+  // import.meta.env // It's wear but to use env has to write twice times here and
   return {
-    ...import.meta.env, // here to work
-  }
-}
+    // ...import.meta.env, // here to work
+    VITE_API_URL: import.meta.env.VITE_API_URL,
+  };
+};
