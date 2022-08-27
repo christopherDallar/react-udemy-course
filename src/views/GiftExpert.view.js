cc96368bd849949533d9d8908eac93f9cc461198
christopherDallar <christopherdallar@gmail.com>
2022-08-27 17:00:35 -0400

    75. Agregar una nueva categoría

--- src/views/GiftExpert.view.js
+++ src/views/GiftExpert.view.js
@@ -3,20 +3,32 @@ import { UiAddCategory } from '../shared/components/ui/UiAddCategory.component';
 import { GifGrid } from './../shared/components/gif/GifGrid.component';
 
 export const ViewGiftExpert = () => {
-	const [categories, setCategories] = useState(['One punch']);
+  const [categories, setCategories] = useState([
+    'One punch',
+  ]);
 
-	return (
-		<>
-			<h1>GiftExpertApp</h1>
-			<UiAddCategory setCategories={setCategories} />
-			<hr></hr>
+  const onAddCategory = () => {
+    // categories.push('valorant');
+    // setCategories(categories);
 
-			<ol>
-				{categories.map((cat, i) => (
-					// <li key={cat + i}>{cat}</li>
-					<GifGrid key={cat + i} category={cat} />
-				))}
-			</ol>
-		</>
-	);
+    setCategories([...categories, 'valorant']); // no muta el estado
+    console.log(categories);
+  };
+
+  return (
+    <>
+      <h1>GiftExpertApp</h1>
+      <UiAddCategory setCategories={setCategories} />
+      <hr></hr>
+
+      <button onClick={onAddCategory}>Add</button>
+
+      <ol>
+        {categories.map((cat, i) => (
+          // <li key={cat + i}>{cat}</li>
+          <GifGrid key={cat + i} category={cat} />
+        ))}
+      </ol>
+    </>
+  );
 };
