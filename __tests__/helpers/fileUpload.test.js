da6642d8797b8f251bcfc3319b42086296d4272d
christopherDallar <christopherdallar@gmail.com>
2022-09-11 18:43:59 -0400

    319. Pruebas de carga de archivos

---
+++ __tests__/helpers/fileUpload.test.js
@@ -0,0 +1,15 @@
+import { fileUpload } from '../../src/helpers/fileUpload'
+
+describe('Testing fileUpload', () => {
+  test('should to upload correctly to cloudinary', async () => {
+    const imgUrl =
+      'https://www.viewhotels.jp/ryogoku/wp-content/uploads/sites/9/2020/03/test-img.jpg'
+    const resp = await fetch(imgUrl)
+    const blob = await resp.blob()
+    const file = new File([blob], 'picture.jpg')
+
+    const url = await fileUpload(file)
+
+    expect(typeof url).toBe('string')
+  })
+})
