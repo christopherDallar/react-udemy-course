d8610284e478394640571766e7322d25c9c04c6e
christopherDallar <christopherdallar@gmail.com>
2022-09-11 19:16:30 -0400

    320. Cloudinary SDK - Delete image

--- src/__tests__/helpers/fileUpload.test.js
+++ src/__tests__/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
 import { fileUpload } from '../../helpers/fileUpload'
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: 'dlnsabgds',
+  api_key: '582412742288218',
+  api_secret: 'grfDeUNCi9RIz_rWWcdnlwgfw2M',
+  secure: true,
+})
 
 describe('Testing fileUpload', () => {
   test('should to upload correctly to cloudinary', async () => {
@@ -11,5 +19,23 @@ describe('Testing fileUpload', () => {
     const url = await fileUpload(file)
 
     expect(typeof url).toBe('string')
+
+    const segments = url.split('/')
+    const imgId = segments[segments.length - 1].replace('.jpg', '')
+
+    const cloudResp = await cloudinary.api.delete_resources(
+      ['jorunal-app/' + imgId],
+      {
+        resource_type: 'image',
+      },
+    )
+  })
+
+  test('should to return null', async () => {
+    const file = new File([], 'picture.jpg')
+
+    const url = await fileUpload(file)
+
+    expect(url).toBe(null)
   })
 })
