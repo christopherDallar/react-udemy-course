e9b18136d9baf06b46a5b3c9fab24de30d2ad592
christopherDallar <christopherdallar@gmail.com>
2022-09-11 19:52:34 -0400

    322. Pruebas faltantes con el authSlice

--- src/__tests__/fixtures/authFixtures.js
+++ src/__tests__/fixtures/authFixtures.js
@@ -24,7 +24,7 @@ export const notAuthenticatedState = {
   email: null,
   displayName: null,
   photoURL: null,
-  errorMessage: null,
+  errorMessage: undefined,
 }
 
 export const demoUser = {
--- src/__tests__/store/auth/authSlice.test.js
+++ src/__tests__/store/auth/authSlice.test.js
@@ -1,7 +1,16 @@
-import { renderHook } from '@testing-library/react'
-import { authSlice } from '../../../store/auth/authSlice'
-
-import { initialState } from '../../fixtures/authFixtures'
+import {
+  authSlice,
+  AuthStatusEnum,
+  checkingCredentials,
+  login,
+  logout,
+} from '../../../store/auth/authSlice'
+import {
+  authenticatedState,
+  demoUser,
+  initialState,
+  notAuthenticatedState,
+} from '../../fixtures/authFixtures'
 
 describe('Testing authSlice', () => {
   test('should to return initialState and must to be called as auth', () => {
@@ -10,5 +19,33 @@ describe('Testing authSlice', () => {
 
     const state = authSlice.reducer(initialState, {})
     expect(state).toEqual(initialState)
+  })
+
+  test('should to return authenticated state', () => {
+    const state = authSlice.reducer(initialState, login(demoUser))
+    expect(state).toEqual(authenticatedState)
+  })
+
+  test('should to return not authenticated state', () => {
+    const state = authSlice.reducer(authenticatedState, logout())
+    expect(state).toEqual(notAuthenticatedState)
+  })
+
+  test('should to return not authenticated state and show error msg', () => {
+    const errorMessage = 'Wrong credentials'
+    const state = authSlice.reducer(
+      authenticatedState,
+      logout({ errorMessage }),
+    )
+
+    notAuthenticatedState.errorMessage = errorMessage
+
+    expect(state).toEqual(notAuthenticatedState)
+  })
+
+  test('should checkingCredentials to state.status to checking', () => {
+    const state = authSlice.reducer(authenticatedState, checkingCredentials())
+
+    expect(state.status).toBe(AuthStatusEnum.checking)
   })
 })
