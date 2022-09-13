addf831e7e6a84de53ecdf17487dbec9864735da
christopherDallar <christopherdallar@gmail.com>
2022-09-12 23:24:12 -0400

    323. Pruebas sobre Thunks

--- src/__tests__/store/auth/thunks.test.js
+++ src/__tests__/store/auth/thunks.test.js
@@ -1,7 +1,18 @@
+import { checkingCredentials } from '../../../store/auth'
 import { checkingAuth } from '../../../store/auth/thunks'
 
+jest.mock('../../../firebase/providers')
+
 describe('Testing auth thunks', () => {
-  test('should to invoke checkingCredentials', () => {
-    checkingAuth()
+  const dispatch = jest.fn()
+
+  beforeEach(() => jest.clearAllMocks())
+
+  test('should to invoke checkingCredentials', async () => {
+    await checkingAuth()(dispatch)
+    // const value = checkingCredentials()
+
+    // console.log(value)
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials())
   })
 })
