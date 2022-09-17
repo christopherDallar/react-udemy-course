1cf782f9a43e645c42ce03e5813858f623bdbd9d
christopherDallar <christopherdallar@gmail.com>
2022-09-17 18:22:12 -0400

    329. Prueba completa sobre inserción

--- src/__tests__/store/journal/thunks.test.js
+++ src/__tests__/store/journal/thunks.test.js
@@ -1,4 +1,11 @@
-import { startNewNote } from '../../../store/journal'
+import { collection, deleteDoc, getDocs } from 'firebase/firestore/lite'
+import { FirebaseDB } from '../../../firebase/config'
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+  startNewNote,
+} from '../../../store/journal'
 
 describe('Testing Journal Thunks', () => {
   const dispatch = jest.fn()
@@ -13,6 +20,31 @@ describe('Testing Journal Thunks', () => {
     })
 
     await startNewNote()(dispatch, getState)
-    expect(dispatch).toHaveBeenCalledWith()
+    expect(dispatch).toHaveBeenCalledWith(savingNewNote())
+    expect(dispatch).toHaveBeenCalledWith(
+      addNewEmptyNote({
+        body: '',
+        title: '',
+        id: expect.any(String),
+        date: expect.any(Number),
+      }),
+    )
+
+    expect(dispatch).toHaveBeenCalledWith(
+      setActiveNote({
+        body: '',
+        title: '',
+        id: expect.any(String),
+        date: expect.any(Number),
+      }),
+    )
+
+    // Drop from firebase
+    const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`)
+    const docs = await getDocs(collectionRef)
+
+    const deletePromises = []
+    docs.forEach((doc) => deletePromises.push(deleteDoc(doc.ref)))
+    await Promise.all(deletePromises)
   })
 })
