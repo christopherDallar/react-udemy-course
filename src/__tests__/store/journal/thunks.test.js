b6f51eb5eb58b27066d82683cf785f2cf63a4f0c
christopherDallar <christopherdallar@gmail.com>
2022-09-17 17:06:48 -0400

    327. Pruebas en Journal Thunks

---
+++ src/__tests__/store/journal/thunks.test.js
@@ -0,0 +1,17 @@
+import { startNewNote } from '../../../store/journal'
+
+describe('Testing Journal Thunks', () => {
+  const dispatch = jest.fn()
+  const getState = jest.fn()
+
+  beforeEach(() => jest.clearAllMocks())
+
+  test('should to create a new note', async () => {
+    const uid = 'TEST-UID'
+    getState.mockReturnValue({
+      auth: { uid },
+    })
+
+    await startNewNote()(dispatch, getState)
+  })
+})
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,105 +1,105 @@
-import { collection, deleteDoc, doc, setDoc } from 'firebase/firestore/lite';
-import { FirebaseDB } from './../../firebase/config';
-import { loadNotes, fileUpload } from './../../helpers';
+import { collection, deleteDoc, doc, setDoc } from 'firebase/firestore/lite'
+import { FirebaseDB } from './../../firebase/config'
+import { loadNotes, fileUpload } from './../../helpers'
 import {
-	addNewEmptyNote,
-	setActiveNote,
-	savingNewNote,
-	setNotes,
-	setSaving,
-	updatedNote,
-	setPhotosToActiveNote,
-	deleteNoteById,
-} from './';
+  addNewEmptyNote,
+  setActiveNote,
+  savingNewNote,
+  setNotes,
+  setSaving,
+  updatedNote,
+  setPhotosToActiveNote,
+  deleteNoteById,
+} from './'
 
 export const startNewNote = () => {
-	return async (dispatch, getState) => {
-		dispatch(savingNewNote());
+  return async (dispatch, getState) => {
+    dispatch(savingNewNote())
 
-		const { uid } = getState().auth;
+    const { uid } = getState().auth
 
-		const newNote = {
-			title: '',
-			body: '',
-			date: new Date().getTime(),
-		};
+    const newNote = {
+      title: '',
+      body: '',
+      date: new Date().getTime(),
+    }
 
-		const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
-		await setDoc(newDoc, newNote);
+    const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`))
+    await setDoc(newDoc, newNote)
 
-		newNote.id = newDoc.id;
+    newNote.id = newDoc.id
 
-		dispatch(addNewEmptyNote(newNote));
-		dispatch(setActiveNote(newNote));
-	};
-};
+    dispatch(addNewEmptyNote(newNote))
+    dispatch(setActiveNote(newNote))
+  }
+}
 
 export const startLoadingNotes = () => {
-	return async (dispatch, getState) => {
-		const { uid } = getState().auth;
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth
 
-		if (!uid) {
-			throw new Error("User's UID doesn't exist");
-		}
+    if (!uid) {
+      throw new Error("User's UID doesn't exist")
+    }
 
-		const notes = await loadNotes(uid);
+    const notes = await loadNotes(uid)
 
-		dispatch(setNotes(notes));
-	};
-};
+    dispatch(setNotes(notes))
+  }
+}
 
 export const startSaveNote = () => {
-	return async (dispatch, getState) => {
-		dispatch(setSaving());
+  return async (dispatch, getState) => {
+    dispatch(setSaving())
 
-		const { uid } = getState().auth;
-		const { active: note } = getState().journal;
+    const { uid } = getState().auth
+    const { active: note } = getState().journal
 
-		const noteToFireStore = { ...note };
-		delete noteToFireStore.id;
+    const noteToFireStore = { ...note }
+    delete noteToFireStore.id
 
-		const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
+    const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`)
 
-		await setDoc(docRef, noteToFireStore, { merge: true });
+    await setDoc(docRef, noteToFireStore, { merge: true })
 
-		dispatch(updatedNote(note));
-	};
-};
+    dispatch(updatedNote(note))
+  }
+}
 
 export const startUploadingFiles = (files = []) => {
-	return async (dispatch) => {
-		dispatch(setSaving());
+  return async (dispatch) => {
+    dispatch(setSaving())
 
-		console.log(files);
+    console.log(files)
 
-		// await fileUpload(files[0]);
-		// files.forEach(async (file) => {
-		//  Una de tras de otra
-		// 	await fileUpload(file);
-		// });
+    // await fileUpload(files[0]);
+    // files.forEach(async (file) => {
+    //  Una de tras de otra
+    // 	await fileUpload(file);
+    // });
 
-		const fileUploadPromises = [];
+    const fileUploadPromises = []
 
-		for (const file of files) {
-			fileUploadPromises.push(fileUpload(file));
-		}
+    for (const file of files) {
+      fileUploadPromises.push(fileUpload(file))
+    }
 
-		// Todas al mismo tiempo
-		const imgUrls = await Promise.all(fileUploadPromises);
+    // Todas al mismo tiempo
+    const imgUrls = await Promise.all(fileUploadPromises)
 
-		console.log(imgUrls);
-		dispatch(setPhotosToActiveNote(imgUrls));
-	};
-};
+    console.log(imgUrls)
+    dispatch(setPhotosToActiveNote(imgUrls))
+  }
+}
 
 export const startDeletingNote = () => {
-	return async (dispatch, getState) => {
-		const { uid } = getState().auth;
-		const { active: note } = getState().journal;
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth
+    const { active: note } = getState().journal
 
-		const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
-		await deleteDoc(docRef);
+    const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`)
+    await deleteDoc(docRef)
 
-		dispatch(deleteNoteById(note.id));
-	};
-};
+    dispatch(deleteNoteById(note.id))
+  }
+}
