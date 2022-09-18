8eeea3d753d46fe61e7bccf6197c29878e6c6bb4
christopherDallar <christopherdallar@gmail.com>
2022-09-18 14:02:27 -0400

    334. Botón de Google debe de llamar startGoogleSignIn

--- src/__tests__/components/auth/pages/LoginScreen.jsx
+++ src/__tests__/components/auth/pages/LoginScreen.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
 import { LoginScreen } from '../../../../components/auth/pages/LoginScreen'
+import { startGoogleSignIn } from '../../../../store/auth'
 import authSlice from '../../../../store/auth/authSlice'
 
 const store = configureStore({
@@ -13,6 +15,14 @@ const store = configureStore({
 
   // }
 })
+
+jest.mock('../../../../store/auth')
+
+// jest.mock('react-redux', () => {
+// useDispatch: () => {
+//   return jest.fn()
+// }
+// })
 
 describe('Testing <LoginScreen />', () => {
   test('should to show component well', () => {
@@ -25,5 +35,21 @@ describe('Testing <LoginScreen />', () => {
     )
 
     expect(screen.getAllByText('Login').length).toBeGreaterThanOrEqual(1)
+  })
+
+  test('should to call onGoogleSignIn', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LoginScreen />
+        </MemoryRouter>
+      </Provider>,
+    )
+
+    const gBtn = screen.getAllByAltText('googleSignInBtn')
+    // screen.debug()
+    fireEvent.click(gBtn)
+
+    // expect(startGoogleSignIn()).toBeTruthy()
   })
 })
--- src/components/auth/pages/LoginScreen.js
+++ src/components/auth/pages/LoginScreen.js
@@ -1,147 +1,141 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react'
+import { Alert, Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useForm } from '../../../hooks/useForm'
 import {
-	Alert,
-	Button,
-	Grid,
-	Link,
-	TextField,
-	Typography,
-} from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useForm } from '../../../hooks/useForm';
-import {
-	AuthStatusEnum,
-	startGoogleSignIn,
-	startLoginWithEmailAndPassword,
-} from '../../../store/auth';
-import { Google } from '@mui/icons-material';
-import { AuthLayout } from './../layout/AuthLayout';
+  AuthStatusEnum,
+  startGoogleSignIn,
+  startLoginWithEmailAndPassword,
+} from '../../../store/auth'
+import { Google } from '@mui/icons-material'
+import { AuthLayout } from './../layout/AuthLayout'
 
 const formData = {
-	email: 'christopher123456@test.com',
-	password: '123456',
-};
+  email: 'christopher123456@test.com',
+  password: '123456',
+}
 
 const formValidations = {
-	email: [(value) => value.includes('@'), 'Email must to be @.'],
-	password: [
-		(value) => value.length >= 6,
-		'Password must to be at least 6 characters.',
-	],
-};
+  email: [(value) => value.includes('@'), 'Email must to be @.'],
+  password: [
+    (value) => value.length >= 6,
+    'Password must to be at least 6 characters.',
+  ],
+}
 
 export const LoginScreen = () => {
-	const { status, errorMessage } = useSelector((state) => state.auth);
-	const dispatch = useDispatch();
+  const { status, errorMessage } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
 
-	const [formSubmitted, setFormSubmitted] = useState(false);
-	const {
-		email,
-		password,
-		onInputChange,
-		isFormValid,
-		emailValid,
-		passwordValid,
-	} = useForm(formData, formValidations);
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const {
+    email,
+    password,
+    onInputChange,
+    isFormValid,
+    emailValid,
+    passwordValid,
+  } = useForm(formData, formValidations)
 
-	const isCheckingAuthentication = useMemo(
-		() => status === AuthStatusEnum.checking,
-		[status]
-	);
+  const isCheckingAuthentication = useMemo(
+    () => status === AuthStatusEnum.checking,
+    [status],
+  )
 
-	const onSubmit = (e) => {
-		e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault()
 
-		setFormSubmitted(true);
+    setFormSubmitted(true)
 
-		if (!isFormValid) {
-			return;
-		}
+    if (!isFormValid) {
+      return
+    }
 
-		dispatch(startLoginWithEmailAndPassword({ email, password }));
-	};
+    dispatch(startLoginWithEmailAndPassword({ email, password }))
+  }
 
-	const onGoogleSignIn = (e) => {
-		dispatch(startGoogleSignIn());
-	};
+  const onGoogleSignIn = (e) => {
+    dispatch(startGoogleSignIn())
+  }
 
-	return (
-		<>
-			<AuthLayout title='Login'>
-				<form onSubmit={onSubmit}>
-					<Grid container>
-						<Grid item xs={12} sx={{ mt: 2 }}>
-							<TextField
-								label='Email'
-								type='email'
-								placeholder='email@gmail.com'
-								fullWidth
-								name='email'
-								value={email}
-								onChange={onInputChange}
-								error={!!emailValid && formSubmitted}
-								helperText={emailValid}
-							/>
-						</Grid>
+  return (
+    <>
+      <AuthLayout title="Login">
+        <form onSubmit={onSubmit}>
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Email"
+                type="email"
+                placeholder="email@gmail.com"
+                fullWidth
+                name="email"
+                value={email}
+                onChange={onInputChange}
+                error={!!emailValid && formSubmitted}
+                helperText={emailValid}
+              />
+            </Grid>
 
-						<Grid item xs={12} sx={{ mt: 2 }}>
-							<TextField
-								label='Password'
-								type='password'
-								placeholder='password'
-								fullWidth
-								name='password'
-								value={password}
-								onChange={onInputChange}
-								error={!!passwordValid && formSubmitted}
-								helperText={passwordValid}
-							/>
-						</Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Password"
+                type="password"
+                placeholder="password"
+                fullWidth
+                name="password"
+                value={password}
+                onChange={onInputChange}
+                error={!!passwordValid && formSubmitted}
+                helperText={passwordValid}
+              />
+            </Grid>
 
-						<Grid item xs={12} display={!!errorMessage ? '' : 'none'}>
-							<Alert severity='error'>{errorMessage}</Alert>
-						</Grid>
+            <Grid item xs={12} display={!!errorMessage ? '' : 'none'}>
+              <Alert severity="error">{errorMessage}</Alert>
+            </Grid>
 
-						<Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-							<Grid item xs={12} sm={6}>
-								<Button
-									variant='contained'
-									fullWidth
-									type='submit'
-									disabled={isCheckingAuthentication}
-								>
-									Login
-								</Button>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								<Button
-									variant='contained'
-									fullWidth
-									onClick={onGoogleSignIn}
-									disabled={isCheckingAuthentication}
-								>
-									<Google />
-									<Typography sx={{ ml: 1 }}>Google</Typography>
-								</Button>
-							</Grid>
-						</Grid>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  type="submit"
+                  disabled={isCheckingAuthentication}
+                >
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  onClick={onGoogleSignIn}
+                  disabled={isCheckingAuthentication}
+                  data-alt-text="googleSignInBtn"
+                >
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
 
-						<Grid container direction='row' justifyContent='end'>
-							<Link component={RouterLink} color='inherit' to='/auth/register'>
-								Sign up new account
-							</Link>
-						</Grid>
-					</Grid>
-				</form>
-			</AuthLayout>
-		</>
-	);
-};
+            <Grid container direction="row" justifyContent="end">
+              <Link component={RouterLink} color="inherit" to="/auth/register">
+                Sign up new account
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </AuthLayout>
+    </>
+  )
+}
 
 // eslint-disable-next-line no-lone-blocks
 {
-	/* <h3 className='auth__title'>Login</h3>
+  /* <h3 className='auth__title'>Login</h3>
 
 <form
 	onSubmit={onSubmit}
