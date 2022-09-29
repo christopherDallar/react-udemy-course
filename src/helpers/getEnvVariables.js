1733f4c830bfc427f24364bf730ae91ba2e25576
christopherDallar <christopherdallar@gmail.com>
2022-09-28 23:38:50 -0400

    Fixed:
    * getting .env values.
    * become from main.jsx
    to tsx main

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,7 +1,9 @@
 export const getEnvVariables = () => {
   // import.meta.env // It's wear but to use env has to write twice times here and
-  return {
-    ...import.meta.env, // here to work
-    // VITE_API_URL: import.meta.env.VITE_API_URL,
-  }
-}
+
+  return import.meta.env;
+  // return {
+  //   ...import.meta.env, // here to work
+  //   // VITE_API_URL: import.meta.env.VITE_API_URL,
+  // };
+};
